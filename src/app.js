41fc587cf63ee1b80eff203d6644626eb7ee94af
gokacinlar <101329870+gokacinlar@users.noreply.github.com>
2024-07-24 19:10:38 +0300

    Fix: Multiple elements in image div, refactor code

--- src/app.js
+++ src/app.js
@@ -3,6 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 let acceptableImageTypes = ["image/jpeg", "image/png", "image/webp"]; // Define the MIME types
+let isImageExists = false;
+
+let imageCss = {
+    width: "100%",
+    height: "auto",
+    objectFit: "contain",
+    margin: "0",
+    inset: "0",
+    position: "absolute",
+    zIndex: "2"
+}
 
 function initFunctions() {
     getImageOrigin();
@@ -12,7 +23,7 @@ function initFunctions() {
 
 function getImageOrigin() {
     const fileUploadBtn = $("#uploadImage");
-    const uploadFileDiv = $("#uploadFile"); // Reference the upload div
+    const uploadFileDiv = $("#uploadFile");
 
     $(fileUploadBtn).on("click", function () {
         $("#theFile").trigger("click"); // Trigger click on hidden file input
@@ -22,7 +33,7 @@ function getImageOrigin() {
     $("#theFile").on("change", function () {
         const file = this.files[0];
         if (!file) {
-            return; // No file selected
+            return;
         }
 
         if (!acceptableImageTypes.includes(file.type)) {
@@ -35,17 +46,14 @@ function getImageOrigin() {
             src: URL.createObjectURL(file),
             alt: file.name,
             class: "uploaded-image"
-        }).css({
-            "width": "100%",
-            "height": "auto",
-            "object-fit": "cover",
-            "margin": "0",
-            "position": "absolute",
-            "z-index": "2",
-            "inset": "0"
-        });
+        }).css(imageCss);
 
-        uploadFileDiv.append(img); // Append the image to the upload div
+        // Append the image to the upload area
+        if (!isImageExists) {
+            uploadFileDiv.append(img);
+            addRemoveImageBtn();
+            isImageExists = true;
+        }
     });
 }
 
@@ -67,20 +75,47 @@ function dragAndDropImage() {
         $(this).removeClass("dragover");
 
         const file = e.originalEvent.dataTransfer.files[0];
+
+        if (!acceptableImageTypes.includes(file.type)) {
+            alert("Invalid image type. Please select a JPEG, PNG, or WEBP image.");
+            console.error("Invalid image type:", file.type);
+            return;
+        }
+
         const img = $("<img>").attr({
             src: URL.createObjectURL(file),
             alt: file.name,
             class: "uploaded-image"
-        });
+        }).css(imageCss);
 
         // Append the image to the upload area
-        $("#uploadFile").append(img);
+        if (!isImageExists) {
+            $("#uploadFile").append(img);
+            addRemoveImageBtn()
+            isImageExists = true;
+        }
     });
 }
 
 /**
  * Helper Functions
  */
+
+let $removeBtn;
+let $removeBtnCss = ["box has-text-light has-background-primary-dark has-text-centered is-size-6 column is-1"];
+
+function addRemoveImageBtn() {
+    $removeBtn = $("<button>");
+    $removeBtn.html("<i class='bi bi-arrow-clockwise'></i> Remove Image")
+        .attr("class", $removeBtnCss);
+    $("#uploadSection").append($removeBtn);
+    $($removeBtn).on("click", removeRemoveBtnAndReset);
+}
+
+function removeRemoveBtnAndReset() {
+    $removeBtn.remove();
+    $("#uploadFile").empty();
+}
 
 function openFileExplorer() {
     $("#theFile").trigger("click");
